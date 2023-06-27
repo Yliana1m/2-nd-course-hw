7dfbf40e16df380d6cdc2191fb86d5465361bea8
Yliana1m <doroninau88@mail.ru>
2023-06-27 19:18:21 +0700

    redacted

--- hw+5/script.js
+++ hw+5/script.js
@@ -6,10 +6,9 @@
 // const numbs = [1, 5, 4, 10, 0, 3];
 
 // for (let i = 0; i < numbs.length; i++) {
-// 	if (numbs[i] >= 10) break;
 // 	console.log(numbs[i]);
+// 	if (numbs[i] >= 10) break;
 // }
-
 
 // Задание 2
 // Дан массив:[1, 5, 4, 10, 0, 3].
